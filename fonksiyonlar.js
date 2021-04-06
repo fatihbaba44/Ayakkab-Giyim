b11e55f30cd1c2ca661ebc4f1baf67d14d41d881
fatihbaba44 <66630180+fatihbaba44@users.noreply.github.com>
2021-04-06 12:46:16 +0300

    Update fonksiyonlar.js

--- fonksiyonlar.js
+++ fonksiyonlar.js
@@ -1,10 +1,24 @@
-<!doctype html>
-<html>
-<head>
-<meta charset="utf-8">
-<title>Başlıksız Belge</title>
-</head>
-
-<body>
-</body>
-</html>
+$(document).ready(function(){
+	
+	$.SoruIcerigiGoster			=	function(ElemanIDsi){
+		var SoruIDsi			=	ElemanIDsi;
+		var IslenecekAlan		=	"#" + ElemanIDsi;
+		$(".SorununCevapAlani").slideUp();
+		$(IslenecekAlan).parent().find(".SorununCevapAlani").slideToggle();
+	}
+	
+	$.UrunDetayResmiDegistir	=	function(Klasor, ResimDegeri){
+		var ResimIcinDosyaYolu	=	"Resimler/UrunResimleri/" + Klasor + "/" + ResimDegeri;
+		$("#BuyukResim").attr("src", ResimIcinDosyaYolu);
+	}
+	
+	$.KrediKartiSecildi			=	function(){
+		$(".BHAlanlari").css("display", "none");
+		$(".KKAlanlari").css("display", "block");
+	}
+	
+	$.BankaHavalesiSecildi			=	function(){
+		$(".KKAlanlari").css("display", "none");
+		$(".BHAlanlari").css("display", "block");
+	}
+});
