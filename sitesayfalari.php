0a91a153f7a2f7d659237be125dc948c75401b8e
fatihbaba44 <66630180+fatihbaba44@users.noreply.github.com>
2021-05-18 18:22:05 +0300

    SİTE SAYFALARI GÜNCELLENDİ!!

---
+++ sitesayfalari.php
@@ -0,0 +1,90 @@
+<?php
+$Sayfa[0]		=	"anasayfa.php";
+$Sayfa[1]		=	"hakkimizda.php";
+$Sayfa[2]       =   "uyeliksozlesmesi.php";
+$Sayfa[3]       =   "kullanimkosullari.php";
+$Sayfa[4]       =   "gizliliksozlesmesi.php";
+$Sayfa[5]       =   "mesafelisatissozlesmesi.php";
+$Sayfa[6]       =   "teslimat.php";
+$Sayfa[7]       =   "iptaliadedegisim.php";
+$Sayfa[8]		=	"bankahesaplarimiz.php";
+$Sayfa[14]      =   "kargomnerede.php";
+$Sayfa[15]      =   "kargomneredesonuc.php";
+$Sayfa[9]      =    "havalebildirimformu.php";
+$Sayfa[10]     =    "havalebildirimformusonuc.php";
+$Sayfa[11]     =    "havalebildirimformusonuceksikalan.php";
+$Sayfa[12]     =    "havalebildirimformusonuchata.php";
+$Sayfa[13]     =    "havalebildirimformusonuctamam.php";
+$Sayfa[16]     =    "iletisim.php";
+$Sayfa[17]     =    "iletisimsonuc.php";
+$Sayfa[18]     =    "iletisimsonuceksikalan.php";
+$Sayfa[19]     =    "iletisimsonuchata.php";
+$Sayfa[20]     =    "iletisimsonuctamam.php";
+$Sayfa[21]     =    "destek.php";
+$Sayfa[22]     =    "yeniuyeol.php";
+$Sayfa[23]     =    "yeniuyeolsonuc.php";
+$Sayfa[24]     =    "yeniuyeolsonucaktivasyontamam.php";
+$Sayfa[25]     =    "yeniuyeolsonuceksikalan.php";
+$Sayfa[26]     =    "yeniuyeolsonuchata.php";
+$Sayfa[27]     =    "yeniuyeolsonuconaysizsozlesme.php";
+$Sayfa[28]     =    "yeniuyeolsonuctamam.php";
+$Sayfa[29]     =    "yeniuyeolsonuctekrarlanan.php";
+$Sayfa[30]     =    "üyecikis.php";
+$Sayfa[31]     =    "uyegiris.php";
+$Sayfa[32]     =    "uyegirissonuc.php";
+$Sayfa[33]     =    "uyegirissonucaktivasyonsuz.php";
+$Sayfa[34]     =    "uyegirissonuceksikalan.php";
+$Sayfa[35]     =    "uyegirissonuchata.php";
+$Sayfa[36]     =    "uyegirissonucyanlisbilgi.php";
+$Sayfa[37]     =    "sifremiunuttum.php";
+$Sayfa[38]     =    "sifremiunuttumsonuc.php";
+$Sayfa[39]     =    "sifremiunuttumsonuceksikalan.php";
+$Sayfa[40]     =    "sifremiunuttumsonuchata.php";
+$Sayfa[41]     =    "sifremiunuttumsonuctamam.php";
+$Sayfa[42]     =    "sifremiunuttumsonucyanlisbilgi.php";
+$Sayfa[43]     =    "yenisifreolustur.php";
+$Sayfa[44]     =    "yenisifreolustursonuc.php";
+$Sayfa[45]     =    "yenisifreolustursonuceksikalan.php";
+$Sayfa[46]     =    "yenisifreolustursonuchata.php";
+$Sayfa[47]     =    "yenisifreolustursonuctamam.php";
+$Sayfa[48]     =    "yenisifreolustursonucuyusmayansifre.php";
+$Sayfa[84]     =    "erkekayakkabilari.php";
+$Sayfa[85]     =    "kadinayakkabilari.php";
+$Sayfa[86]     =    "cocukayakkabilari.php";
+$Sayfa[87]     =    "alisverissepeti.php";
+$Sayfa[88]     =    "alisverisepetiadresvekargosecimi.php";
+$Sayfa[89]     =    "alisverissepetibankahavalesiileodemehata.php";
+$Sayfa[90]     =    "alisverissepetibankahavalesiileodemetamam.php";
+$Sayfa[91]     =    "alisverissepetikredikartiodeme.php";
+$Sayfa[92]     =    "alisverissepetikredikartiodemesonuc.php";
+$Sayfa[93]     =    "alisverissepetikredikartiodemesonuchata.php";
+$Sayfa[94]     =    "alisverissepetikredikartiodemesonuctamam.php";
+$Sayfa[95]     =    "alisverissepetiodemesecimi.php";
+$Sayfa[96]     =    "alisverissepetiodemesecimisonuc.php";
+$Sayfa[97]     =    "alisverissepetiurunadetarttir.php";
+$Sayfa[98]     =    "alisverissepetiurunadetazalt.php";
+$Sayfa[99]     =    "alisverissepetiurunsil.php";
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
